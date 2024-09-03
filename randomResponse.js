7a14ff33e97ac4347a8d1560f900e7c4424baeee
xXKalleKaninXx <jakob.boaz@mac.com>
2024-09-03 15:45:53 +0200

    Added a function for testing

---
+++ randomResponse.js
@@ -0,0 +1,12 @@
+const justCheck = 0;
+
+const startMessage = ["Its looking like ","It all began ","Then there was ","Suprisingly it was ","Nobody saw that it was "];
+let testVar = "Ello!";
+
+const testsomething = (starter, stopper) => {
+    if (1+ 2 === 4) {
+        return "The world is ending";
+    } else {return starter + stopper}
+}
+
+console.log(testsomething(startMessage[Math.floor(Math.random()*startMessage.length)],testVar))
