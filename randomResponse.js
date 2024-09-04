8496c45b70a8ab4e86a241345e5ea20f62738a6c
xXKalleKaninXx <jakob.boaz@mac.com>
2024-09-04 14:03:27 +0200

    trying to make it more fancy

--- randomResponse.js
+++ randomResponse.js
@@ -1,23 +1,28 @@
 
+const phraseMethod = (partArray) => {
+    return {
+        partInfo : partArray,
+        partLength : partArray.length,
+        randomPart() {
+            return this.partInfo[Math.floor(Math.random()*this.partLength)];
+        },
+
+    }
+}
+
+const combinedParts = [];
 
 
-const startMessage = ["Everbody agrees that","Nobody thinks that","And thus, no one foresaw that",
+const startMessage2 = phraseMethod(["Everbody agrees that","Nobody thinks that","And thus, no one foresaw that",
     "Why is it that","Hopefully, nobody notices that","Dont you hate it that","They yelled that",
-    "It was foretold that","Dont you love it that","You always say that"];
-const randomStartPart = startMessage[Math.floor(Math.random()*startMessage.length)];
+    "It was foretold that","Dont you love it that","You always say that"]);
 
-const middleMessage = ["bears","turtles","pigeons","cats","dogs","elephants","tigers","rhinoceroses","whales","sharks"];
-const randomMiddlePart = middleMessage[Math.floor(Math.random()*middleMessage.length)];
+const middleMessage2 = phraseMethod(["bears","turtles","pigeons","cats","dogs","elephants","tigers","rhinoceroses","whales","sharks"]);
 
-const stopMessage = ["are cool.","are fancy.","are smart.","are badass.","are tha bomb.","are okay.",
-    "are mid.","are the bestest","are my favorite","are right behind you."];
-const randomStopPart = stopMessage[Math.floor(Math.random()*stopMessage.length)];
+const stopMessage2 = phraseMethod(["are cool.","are fancy.","are smart.","are badass.","are tha bomb.","are okay.",
+    "are mid.","are the bestest","are my favorite","are right behind you."]);
 
 
-const randomStringGenerator = (startPart, middlePart, stopPart) => `${startPart} ${middlePart} ${stopPart}`;
+combineParts(startMessage2.randomPart(),middleMessage2.randomPart(),stopMessage2.randomPart());
 
-
-console.log(randomStringGenerator(randomStartPart, randomMiddlePart, randomStopPart));
-
-const why = 42;
 
